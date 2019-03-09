9500a7aa5abbe9aaaad3581caa9e1820e20d83bc
Huanjia Liang <huanjial@uci.edu>
2019-03-09 14:06:06 -0800

    Initial Commit

---
+++ parkingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parkingApp
+//
+//  Created by Huanjia Liang on 3/9/19.
+//  Copyright © 2019 Huanjia Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
